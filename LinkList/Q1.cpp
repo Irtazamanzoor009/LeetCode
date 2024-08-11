fdaad0b11729a10e25969500db9165278f65b923
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-08-11 22:19:34 +0500

    link list

--- LinkList/Q1.cpp
+++ LinkList/Q1.cpp
@@ -25,6 +25,23 @@ public:
         head = nullptr;
         tail = nullptr;
     }
+
+    int operator[](int index)
+    {
+        Node *temp = head;
+        int count = 0;
+        while (temp != nullptr)
+        {
+            if (count == index)
+            {
+                return temp->data;
+            }
+            temp = temp->next;
+            count++;
+        }
+        throw out_of_range("Index out of bounds");
+    }
+
     void InsertAtTail(int value)
     {
         Node *temp = new Node(value);
@@ -211,13 +228,102 @@ public:
 
     void RemoveMiddle()
     {
-        Node* temp1 = head;
-        Node* temp2 = head;
+        Node *temp1 = head;
+        Node *temp2 = head;
+        Node *temp3 = head;
         int count = 0;
-        while(temp1->next != nullptr)
+        int count2 = 0;
+        while (temp1->next != nullptr)
         {
-            
+            temp1 = temp1->next;
+            if (count == 2)
+            {
+                temp2 = temp2->next;
+                count = 0;
+                if (count2 == 1)
+                {
+                    temp3 = temp3->next;
+                    count2 = 0;
+                }
+                count2++;
+            }
+            count++;
         }
+
+        temp3->next = temp2->next;
+        delete temp2;
+    }
+
+    Node *getHead()
+    {
+        return head;
+    }
+
+    void PrintRecursively(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            cout << "NULL" << endl;
+            return;
+        }
+        cout << temp->data << "------";
+        PrintRecursively(temp->next);
+    }
+
+    void PrintReverseRecursively(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            return;
+        }
+        PrintReverseRecursively(temp->next);
+        cout << temp->data << "------";
+    }
+
+    int FindMiddle()
+    {
+        Node *temp1 = head;
+        Node *temp2 = head;
+        int count = 0;
+        while (temp1 != nullptr)
+        {
+            temp1 = temp1->next;
+            if (count == 2)
+            {
+                temp2 = temp2->next;
+                count = 0;
+            }
+            count++;
+        }
+        return temp2->data;
+    }
+
+    bool CheckCircular()
+    {
+        if (head == nullptr && tail == nullptr)
+        {
+            return false;
+        }
+        if (tail->next = head)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    Node *ReverseLinkList()
+    {
+        Node *current = head;
+        Node *previous = nullptr;
+        Node *next = nullptr;
+        while(current != nullptr)
+        {
+            next = current->next;
+            current->next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
     }
 
     void Display()
@@ -232,19 +338,90 @@ public:
     }
 };
 
+class Stack
+{
+private:
+    LinkList l;
+
+public:
+    Stack()
+    {
+    }
+
+    void Push(int v)
+    {
+        l.InsertAtTail(v);
+    }
+
+    int operator[](int index)
+    {
+        return l[index];
+    }
+
+    int Pop()
+    {
+        int last = l.GetTail();
+        l.RemoveTail();
+        return last;
+    }
+
+    bool isEmpty()
+    {
+        return l.isEmpty();
+    }
+
+    void Display()
+    {
+        l.Display();
+    }
+};
+
+class Queue
+{
+private:
+    LinkList l;
+
+public:
+    Queue()
+    {
+    }
+    void Enque(int value)
+    {
+        l.InsertAtTail(value);
+    }
+
+    int operator[](int index)
+    {
+        return l[index];
+    }
+
+    int Deque()
+    {
+        int front = l.GetFront();
+        l.RemoveFront();
+        return front;
+    }
+
+    void Display()
+    {
+        l.Display();
+    }
+
+    bool isEmpty()
+    {
+        return l.isEmpty();
+    }
+};
+
 int main()
 {
     LinkList l;
     l.InsertAtTail(1);
     l.InsertAtTail(2);
     l.InsertAtTail(3);
-    l.InsertAtMid(4);
-    l.InsertBefore(10, 1);
-    l.InsertAfter(11, 2);
-    l.Display();
-    cout << endl;
-    cout << l.RemoveTail();
-    cout << endl;
-    l.Display();
-    cout << l.GetTail();
+    l.InsertAtTail(4);
+    l.InsertAtTail(5);
+    l.InsertAtTail(6);
+    Node head = *l.ReverseLinkList();
+    l.PrintRecursively(&head);
 }
