c8e1281836a51b9f45f308e5bc9b5cf0bfe12bf7
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-08-12 23:21:38 +0500

    practice

--- DoublyLinkList/doubleLinkList.cpp
+++ DoublyLinkList/doubleLinkList.cpp
@@ -46,4 +46,172 @@ public:
         }
         size++;
     }
+
+    void InsertAtHead(int value)
+    {
+        Node *temp = new Node(value);
+        if (head == nullptr && tail == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {
+            temp->next = head;
+            head->previous = temp;
+            head = temp;
+        }
+        size++;
+    }
+
+    bool isEmpty()
+    {
+        if(head == nullptr && tail == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void InsertAfter(int value, int key)
+    {
+        Node *new_node = new Node(value);
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            if (temp->data == key)
+            {
+                break;
+            }
+            temp = temp->next;
+        }
+        if (temp == nullptr)
+        {
+            return;
+        }
+        else
+        {
+            if(temp == tail)
+            {
+                InsertAtTail(value);
+            }
+            else
+            {
+                new_node->next = temp->next;
+                temp->next->previous = new_node;
+                temp->next = new_node;
+                new_node->previous = temp;
+            }
+            size++;
+        }
+    }
+
+    void InsertBefore(int value, int key)
+    {
+        Node* temp = head;
+        Node* new_node = new Node(value);
+        while(temp != nullptr)
+        {
+            if(temp->data == key)
+            {
+                break;
+            }
+            temp = temp->next;
+        }
+        if(temp == nullptr)
+        {
+            return;
+        }
+        else
+        {
+            if(temp == head)
+            {
+                InsertAtHead(value);
+            }
+            else
+            {
+                new_node->next = temp;
+                temp->previous->next = new_node;
+                new_node->previous = temp->previous;
+                temp->previous = new_node;
+            }
+            size++;
+        }
+    }
+
+    int GetFront()
+    {
+        return head->data;
+    }
+
+    int GetTail()
+    {
+        return tail->data;
+    }
+
+    void RemoveFront()
+    {
+        if(head == tail)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            head->next->previous = nullptr;
+            head = head->next;
+        }
+        size--;
+    }
+
+    void RemoveEnd()
+    {
+        if(head == tail)
+        {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else
+        {
+            
+        }
+    }
+
+    void Display()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << "-------";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void DisplayReverse()
+    {
+        Node* temp = tail;
+        while(temp != nullptr)
+        {
+            cout << temp->data << "-------";
+            temp = temp->previous;
+        }
+        cout << "NULL" << endl;
+    }
 };
+
+int main()
+{
+    DoubleLinkList l;
+    l.InsertAtTail(1);
+    l.InsertAtTail(2);
+    l.InsertAtTail(3);
+    l.InsertAtTail(4);
+    l.InsertAtTail(5);
+    l.RemoveFront();
+    l.Display();
+    cout << l.GetFront();
+}
