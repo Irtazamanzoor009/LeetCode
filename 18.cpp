a42334b91bab6bf9bb17a5f0419be4de36365355
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-08-16 19:56:06 +0500

    p

---
+++ 18.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include"linklist.h"
+using namespace std;
+
+ListNode *swapPairs(ListNode *head)
+{
+    ListNode* temp = head;
+    while(head->next == nullptr && head->next->next == nullptr)
+    {
+        int temp1 = temp->data;
+    }
+}
+
+int main()
+{
+
+}
